3148a9e36723c9871a1fc6c34ed73a46ca91893d
Thibaud DESODT <thibaud.desodt@dgtresor.gouv.fr>
2017-07-14 13:57:22 +0200

    Initial commit

---
+++ NCrunchBugTest/NCrunchFailingTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace NCrunchBugTest
+{
+    public class ArticleContents
+    {
+        public string Author { get; set; }
+    }
+
+    public class NCrunchFailingTest
+    {
+        public static IEnumerable<object[]> ArticleContentsEqualityExamples()
+        {
+            var fixture = new Fixture();
+            var original = fixture.Create<ArticleContents>();
+            var clone = Clone(original);
+            // returning json contents so that xunit properly handle multi values (needs deserialization etc)
+            yield return new object[] { JsonConvert.SerializeObject(original), JsonConvert.SerializeObject(clone), "Identical clone" };
+        }
+
+        [Theory]
+        [MemberData(nameof(ArticleContentsEqualityExamples))]
+        public void ArticleContents_must_remain_equal_when_changing_unimportant_parts(string originalJson, string modifiedCloneJson, string reason)
+
+        {
+            // what is here does not matter
+        }
+
+        private static T Clone<T>(T original)
+        {
+            // hacky way of cloning serialize/deserialize :p
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(original));
+        }
+    }
+}
---
+++ NCrunchBugTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NCrunchBugTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NCrunchBugTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fc499f8-de74-45ae-ab51-0fe7dda4616b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
